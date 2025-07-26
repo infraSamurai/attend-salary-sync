844a95198ec8125ef2c45aa0c5699a9983a072b1
infraSamurai <devansh.prakhar@gmail.com>
2025-07-26 15:47:21 +0530

    Configure Vercel deployment - fix build output directory

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   root: "frontend",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
   server: {
     host: "::",
     port: 8080,
